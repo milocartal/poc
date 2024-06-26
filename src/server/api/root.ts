b3bb8ca6997875a8f8acb82e69111b6fa17a86f2
Milo CARTAL <105420069+milocartal@users.noreply.github.com>
2024-06-26 09:05:31 +0200

    redo database to macth

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { audioRouter } from "./routers/audio";
+import { photoRouter } from "./routers/photo";
+import { reponseRouter } from "./routers/reponse";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +9,9 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  audio: audioRouter,
+  photo: photoRouter,
+  reponse: reponseRouter,
 });
 
 // export type definition of API
